1186409d299587ba16f48e0ea9db74cf861ecf4a
jmp1234 <johnnypham944@yahoo.com>
2019-03-26 17:53:37 -0700

    completed react intro

--- src/index.js
+++ src/index.js
@@ -1,7 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const luckyNumber = () => Math.floor(Math.random() * 1000) + 1;
+
+
+const greeting = user => {
+  return (
+    <div className="container">
+      <h1>Hello {user.name}</h1>
+      <h2 className="text-muted">Your lucky number is {user.luckyNumber}</h2>
+    </div>
+  )
+}
+
+const person = {
+  name: 'Johnny',
+  luckyNumber: luckyNumber()
+}
+
 ReactDOM.render(
-    <h1 className="container">React Prototypes!</h1>,
+    <h1 className="container">{greeting(person)}</h1>,
     document.getElementById('root')
 );
