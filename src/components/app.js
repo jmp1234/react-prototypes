a56885c8db2347d458bf894fd38b907e885fd435
jmp1234 <johnnypham944@yahoo.com>
2019-03-26 21:35:05 -0700

    created student grade table

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,29 @@
+import React from 'react';
+import Table from './table';
 
+export default () => {
+  return (
+    <div className="container">
+      <h1>Student Grade Table</h1>
+      <Table data={students}/>
+    </div>
+  )
+}
 
+const students = [
+  {
+    name: 'bob',
+    course: 'math',
+    grade: 20
+  },
+  {
+    name: 'bobby',
+    course: 'mathed',
+    grade: 40
+  },
+  {
+    name: 'bobber',
+    course: 'maths',
+    grade: 60
+  }
+];
