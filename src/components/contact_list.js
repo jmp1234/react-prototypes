4c1c158b85df210256d31d44df0ed43168519cc4
jmp1234 <johnnypham944@yahoo.com>
2019-03-31 09:33:53 -0700

    created the address book

---
+++ src/components/contact_list.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+import ContactCard from './contact_card';
+
+
+class ContactList extends Component{
+  // constructor(props) {
+  //   super(props);
+  //
+  //   this.state = {
+  //     contacts: contactData
+  //   }
+  // }
+
+
+
+  render() {
+
+    const list = this.props.contacts.map( (item, index) => {
+      return(
+        <ContactCard key={index} contact={item}/>
+      )
+    });
+
+    return(
+      <div className="col-8">
+        <div className="row">
+          {list}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default ContactList;
---
+++ src/data/contacts.js
@@ -0,0 +1,23 @@
+export default [
+  {
+    firstName: 'Johnny',
+    lastName: 'Pham',
+    phone: '911',
+    email: 'johnnypham444@gmail.com'
+  },
+  {
+    firstName: 'John',
+    lastName: 'Fam',
+    phone: '9111',
+    email: 'johnnypham444@yahoo.com'
+  },
+  {
+    firstName: 'Jonny',
+    lastName: 'Ham',
+    phone: '9112',
+    email: 'johnnypham444@hotmail.com'
+  },
+
+
+
+];
